e6c019c493f0c629884b5eed3878579b7ecdf21a
dimabresky <dimabresky@gmail.com>
2017-06-28 16:19:02 +0300

    Добавлен класс для работы с таблицей продолжительности

---
+++ lib/stores/Duration.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace travelsoft\booking\stores;
+
+use travelsoft\booking\adapters\Highloadblock;
+
+/**
+ * Класс для работы с таблицей продолжительности услуги
+ *
+ * @author dimabresky
+ * @copyright (c) 2017, travelsoft
+ */
+class Duration extends Highloadblock {
+
+    protected static $storeName = 'duration';
+
+}
