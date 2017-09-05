fe5f861d42dbb5522fc9e6e4a5ac4e85495b5b24
dimabresky <dimabresky@gmail.com>
2017-09-05 15:41:33 +0300

    PaymentHistory class is added

---
+++ lib/crm/stores/PaymentHistory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace travelsoft\booking\crm\stores;
+
+use travelsoft\booking\adapters\Highloadblock;
+
+/**
+ * Класс для работы с таблицей истории платежей
+ *
+ * @author dimabresky
+ * @copyright (c) 2017, travelsoft
+ */
+class PaymentHistory extends Highloadblock {
+
+    protected static $storeName = 'paymentHistory';
+
+}
